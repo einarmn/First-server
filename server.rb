9d0a49398bc74e29c214b4972b5665de228b143b
Einar Montalvo <einar.mn2407@gmail.com>
2017-07-20 19:33:44 -0400

    hello.txt in the root

--- server.rb
+++ server.rb
@@ -1,13 +1,9 @@
 require 'sinatra'
 
 get '/' do
-	"Hello world"
+	File.read(File.join('public', 'hello.txt'))
 end
 
 get "/sinatra" do
-	"Hello Sinatra"
-end
-
-get "/si" do 
 	"Hello Sinatra"
 end
